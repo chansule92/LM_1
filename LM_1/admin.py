ae414bb9fea969b5bf0897787d788eb2666b8c2e
chansule92 <57983744+chansule92@users.noreply.github.com>
2020-10-17 04:29:45 +0900

    database

--- LM_1/admin.py
+++ LM_1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import team, player
+from .models import team, player, game, game_detail
 
 class teamAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'league', 'short','nation' ]
@@ -9,6 +9,18 @@ class playerAdmin(admin.ModelAdmin):
     list_display = ['id', 'nick', 'name', 'position','team' ]
     search_fields = ['name', 'league','short']
 
+class gameAdmin(admin.ModelAdmin):
+    list_display = ['date', 'blue', 'purple', 'time','winner','loser' ]
+    search_fields = ['date', 'blue','purple']
+
+
+class game_detailAdmin(admin.ModelAdmin):
+    list_display = ['nick', 'champion', 'Kill', 'death','assist' ]
+    search_fields = ['nick', 'champion']
+
+
 
 admin.site.register(team, teamAdmin)
 admin.site.register(player, playerAdmin)
+admin.site.register(game, gameAdmin)
+admin.site.register(game_detail, game_detailAdmin)
--- LM_1/models.py
+++ LM_1/models.py
@@ -15,3 +15,39 @@ class player(models.Model):
     name = models.CharField(max_length=20)
     position = models.CharField(max_length=20)
     nation = models.CharField(max_length=20)
+
+
+
+class game(models.Model):
+    number = models.AutoField(primary_key = True)
+    date =  models.DateField()
+    blue = models.CharField(max_length=20)
+    purple = models.CharField(max_length=20)
+    time = models.TimeField()
+    winner  = models.CharField(max_length=20)
+    loser = models.CharField(max_length=20)
+
+
+class game_detail(models.Model):
+    number = models.IntegerField(10)
+    nick = models.CharField(max_length=20)
+    champion = models.CharField(max_length=20)
+    Kill = models.IntegerField(3)
+    death = models.IntegerField(3)
+    assist = models.IntegerField(3)
+    CS = models.IntegerField(4)
+    CS_in_teamjungle = models.IntegerField(3)
+    CS_in_enemyjungle = models.IntegerField(3)
+    gold = models.IntegerField(10)
+    vision = models.IntegerField(3)
+    ward_place = models.IntegerField(3)
+    ward_destroyed = models.IntegerField(3)
+    Pinkward_purchase = models.IntegerField(3)
+    champion_damage = models.IntegerField(10)
+    solo_kill = models.IntegerField(2)
+    GD_15 = models.IntegerField(10)
+    CSD_15 = models.IntegerField(10)
+    XPD_15 = models.IntegerField(10)
+    LVLD_15 = models.IntegerField(10)
+    turet_damage = models.IntegerField(10)
+    taken_damage = models.IntegerField(10)
